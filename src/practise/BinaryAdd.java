2d0b2001f1d14197288ede2e9a4b0f04ee85ff5a
Arifur Rahman <arifur.rahman0806@gmail.com>
2023-10-02 13:02:36 +0600

    Hashmap Basic

--- src/practise/BinaryAdd.java
+++ src/practise/BinaryAdd.java
@@ -1,34 +1,34 @@
-package practise;
-
-public class BinaryAdd {
-	public String addBinary(String a,String b) {
-		StringBuilder sb=new StringBuilder();
-		int i=a.length()-1;
-		int j=b.length()-1;
-		int carry=0;
-		
-	    while(i>=0 || j>=0) {
-	    	int sum=carry;
-	    	if(i>=0) sum+=a.charAt(i)-'0';
-	    	if(j>=0) sum+=a.charAt(j)-'0';
-	    	sb.append(sum%2);
-	    	carry=sum/2;
-	    	i--;
-	    	j--;
-	    }
-	    if(carry!=0) sb.append(carry);
-	    return sb.reverse().toString();		
-	}
-	
-	
-
-	public static void main(String[] args) {
-		
-		BinaryAdd num=new BinaryAdd();
-	String output=	num.addBinary("10", "11");
-	System.out.println(output);
-    
-	}
-	
-
-}
+//package practise;
+//
+//public class BinaryAdd {
+//	public String addBinary(String a,String b) {
+//		StringBuilder sb=new StringBuilder();
+//		int i=a.length()-1;
+//		int j=b.length()-1;
+//		int carry=0;
+//		
+//	    while(i>=0 || j>=0) {
+//	    	int sum=carry;
+//	    	if(i>=0) sum+=a.charAt(i)-'0';
+//	    	if(j>=0) sum+=a.charAt(j)-'0';
+//	    	sb.append(sum%2);
+//	    	carry=sum/2;
+//	    	i--;
+//	    	j--;
+//	    }
+//	    if(carry!=0) sb.append(carry);
+//	    return sb.reverse().toString();		
+//	}
+//	
+//	
+//
+//	public static void main(String[] args) {
+//		
+//		BinaryAdd num=new BinaryAdd();
+//	String output=	num.addBinary("10", "11");
+//	System.out.println(output);
+//    
+//	}
+//	
+//
+//}
---
+++ src/practise/HashMapLe.java
@@ -0,0 +1,22 @@
+package practise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapLe {
+
+	public static void main(String[] args) {
+		HashMap<String, Integer> map=new HashMap<String, Integer>();
+		map.put("Arif", 10);
+		map.put("zrif", 20);
+		map.put("srif", 50);
+		map.put("Afrid", 30);
+		for(Map.Entry <String,Integer> e: map.entrySet() ) {
+			System.out.println(e.getKey() +":" + e.getValue());
+		}
+		//System.out.println(map);
+		
+
+	}
+
+}
